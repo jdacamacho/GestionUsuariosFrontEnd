48463cf83416c0cfa81f582d49ff10bc6535bc9e
Julian Camacho <jdacamacho@unicauca.edu.co>
2023-12-24 12:54:26 -0500

    header login y footer

---
+++ src/app/homes/home-administrador/home-administrador.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-administrador',
+  templateUrl: './home-administrador.component.html',
+  styleUrl: './home-administrador.component.css'
+})
+export class HomeAdministradorComponent {
+
+}
