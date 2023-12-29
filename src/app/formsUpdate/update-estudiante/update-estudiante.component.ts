75796babb8465e3596072e96df7cbf4e43fb5718
Julian Camacho <jdacamacho@unicauca.edu.co>
2023-12-29 10:23:53 -0500

    create y update

---
+++ src/app/formsUpdate/update-estudiante/update-estudiante.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-estudiante',
+  templateUrl: './update-estudiante.component.html',
+  styleUrl: './update-estudiante.component.css'
+})
+export class UpdateEstudianteComponent {
+
+}
